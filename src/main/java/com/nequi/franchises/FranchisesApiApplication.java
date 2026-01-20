97633ee7265889e59547edd33ac522f61612a520
jairo.sotelo_pragma <jsoteldo_pragma@mercantilbanco.com.pa>
2026-01-20 06:58:54 -0500

    Base project created

---
+++ src/main/java/com/nequi/franchises/FranchisesApiApplication.java
@@ -0,0 +1,12 @@
+package com.nequi.franchises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchisesApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FranchisesApiApplication.class, args);
+  }
+}
